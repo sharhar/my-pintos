72527decfff9540c9548f81bd85389bd6bf973d4
sharhar <wiishahar@gmail.com>
2022-10-02 00:54:17 +0000

    Very minor bug fix

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,8 +6,9 @@
 #include "userprog/process.h"
 #include <lib/kernel/console.h>
 #include <threads/vaddr.h>
+#include <devices/shutdown.h>
 
-#define SYSCALL_ENTRY(NUM, FUNC, ARGNUM) case NUM: check_user_pointer(args, ARGNUM * 4 + 3); FUNC(args, &f->eax); break;
+#define SYSCALL_ENTRY(NUM, FUNC, ARGNUM) case NUM: check_user_pointer((char*)args, ARGNUM * 4 + 3); FUNC(args, &f->eax); break;
 
 static void syscall_handler(struct intr_frame*);
 
@@ -28,7 +29,7 @@ static void syscall_exit(uint32_t* args, uint32_t* f_eax) {
   NOT_REACHED();
 }
 
-static void syscall_write(uint32_t* args, uint32_t* f_eax) {
+static void syscall_write(uint32_t* args, UNUSED uint32_t* f_eax) {
   if(args[1] == 1) {
     putbuf((void*)args[2], args[3]);
   }
@@ -38,19 +39,27 @@ static void syscall_practice(uint32_t* args, uint32_t* f_eax) {
   *f_eax = args[1] + 1;
 }
 
+static void syscall_halt(uint32_t* args, uint32_t* f_eax) {
+  shutdown_power_off();
+  NOT_REACHED();
+}
+
 void syscall_init(void) { intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall"); }
 
-static void syscall_handler(struct intr_frame* f UNUSED) {
+static void syscall_handler(struct intr_frame* f) {
   struct process* proc = thread_current()->pcb;
 
   proc->in_syscall = true;
 
   uint32_t* args = ((uint32_t*)f->esp);
 
+  check_user_pointer((char*)args, 3);
+
   switch (args[0]) {
     SYSCALL_ENTRY(SYS_EXIT, syscall_exit, 1)
     SYSCALL_ENTRY(SYS_WRITE, syscall_write, 3)
     SYSCALL_ENTRY(SYS_PRACTICE, syscall_practice, 1)
+    SYSCALL_ENTRY(SYS_HALT, syscall_halt, 0)
   }
 
   proc->in_syscall = false;
