5a219ece1e4c477eef3c99851eb1ab85f9302a00
sharhar <wiishahar@gmail.com>
2022-12-01 17:43:41 +0000

    Fixed some bugs, more bugs to go

--- src/devices/block.c
+++ src/devices/block.c
@@ -10,7 +10,7 @@
 #include "devices/ide.h"
 #include "threads/malloc.h"
 
-#define BUFFER_CACHE_SIZE 64
+#define BUFFER_CACHE_SIZE 1
 #define BUFFER_CACHE_PAGE_COUNT DIV_ROUND_UP(BUFFER_CACHE_SIZE * BLOCK_SECTOR_SIZE, PGSIZE)
 
 #define NO_SECTOR ((block_sector_t)-1)
@@ -19,6 +19,7 @@
 #define MAPPED_BIT ((uint8_t)2)
 
 struct buffer_cache_entry {
+  struct block* block;
   block_sector_t sector;
   uint64_t last_used;
   uint8_t flags;
@@ -53,6 +54,7 @@ static struct block* list_elem_to_block(struct list_elem*);
 static uint32_t next_block_device_id;
 static uint64_t usage_counter;
 static struct lock buffer_cache_lock;
+static struct condition buffer_cache_cond;
 static uint8_t* buffer_cache_data;
 static struct buffer_cache_entry buffer_cache[BUFFER_CACHE_SIZE];
 
@@ -60,10 +62,12 @@ void block_init(void) {
   next_block_device_id = 1;
   usage_counter = 1;
   lock_init(&buffer_cache_lock);
+  cond_init(&buffer_cache_cond);
   buffer_cache_data = palloc_get_multiple(0, BUFFER_CACHE_PAGE_COUNT);
 
   for(int i = 0; i < BUFFER_CACHE_SIZE; i++) {
     cond_init(&buffer_cache[i].cond);
+    buffer_cache[i].block = NULL;
     buffer_cache[i].sector = NO_SECTOR;
     buffer_cache[i].last_used = 0;
     buffer_cache[i].flags = 0;
@@ -137,73 +141,83 @@ static void check_sector(struct block* block, block_sector_t sector) {
   }
 }
 
-static struct buffer_cache_entry* find_entry(struct block* block, block_sector_t sector, bool* must_evict) {
-  ASSERT(lock_held_by_current_thread(&buffer_cache_lock));
-
-  uint64_t least_time = ((uint64_t)-1);
-  struct buffer_cache_entry* lru_entry = NULL;
-
-  *must_evict = false;
-
-  for(int i = 0; i < BUFFER_CACHE_SIZE; i++) {
-    if(buffer_cache[i].sector == sector)
-      return &buffer_cache[i];
-
-    if(buffer_cache[i].last_used < least_time && !(buffer_cache[i].flags & MAPPED_BIT)) {
-      least_time = buffer_cache[i].last_used;
-      lru_entry = &buffer_cache[i];
-    }
-  }
-
-  *must_evict = true;
-
-  return lru_entry;
-}
-
 void* block_map_sector(struct block* block, block_sector_t sector, bool coherent) {
-  if(block->type != BLOCK_RAW)
-    return block_map_sector(block->provider, sector + ((uint64_t)block->aux), coherent);
+  check_sector(block, sector);
 
-  lock_acquire(&buffer_cache_lock);
+  if(!lock_held_by_current_thread(&buffer_cache_lock))
+    lock_acquire(&buffer_cache_lock);
 
-  bool must_evict;
-  block_sector_t prev_sector = NO_SECTOR;
-  bool write_back = false;
-  struct buffer_cache_entry* entry = find_entry(block, sector, &must_evict);
+  void* result = NULL;
+  if(block->type != BLOCK_RAW) {
+    /* This block device does not point to a physical device, so we call
+       block_map_sector on the block device backing this one */
+    result = block_map_sector(block->provider, sector + ((uint64_t)block->aux), coherent);
+  } else {
+    /* This block device is backed by a physical device. */
 
-  if(must_evict) {
-    if(entry->flags & MAPPED_BIT)
-      PANIC("Trying to evict mapped buffer cache entry!");
+    // Search for the cache entry that will be used
+    uint64_t least_time = ((uint64_t)-1);
+    struct buffer_cache_entry* lru_entry = NULL;
+    struct buffer_cache_entry* entry = NULL;
+    for(int i = 0; i < BUFFER_CACHE_SIZE; i++) {
+      // If sector is already mapped, return the entry
+      if(buffer_cache[i].block == block && buffer_cache[i].sector == sector) {
+        // If entry is currently mapped, wait for it to be unmapped
+        while(buffer_cache[i].flags & MAPPED_BIT)
+          cond_wait(&buffer_cache[i].cond, &buffer_cache_lock);
+        
+        entry = &buffer_cache[i];
+        break;
+      }
 
-    if(entry->flags & DIRTY_BIT) write_back = true;
-    prev_sector = entry->sector;
-    entry->sector = sector;
-    entry->flags = 0;
+      // Keep track of least recently used entry that is not currently being used
+      if(buffer_cache[i].last_used < least_time && !(buffer_cache[i].flags & MAPPED_BIT)) {
+        least_time = buffer_cache[i].last_used;
+        lru_entry = &buffer_cache[i];
+      }
+    }
+
+    // If no matching entry was found, evict least recently used entry
+    bool write_back = false;
+    bool must_evict = false;
+    block_sector_t prev_sector = NO_SECTOR;
+    struct block* prev_block = NULL;
+    if(entry == NULL) {
+      must_evict = true;
+      entry = lru_entry;
+
+      if(entry->flags & DIRTY_BIT) write_back = true;
+
+      prev_block = entry->block;
+      prev_sector = entry->sector;
+      entry->block = block;
+      entry->sector = sector;
+      entry->flags = 0;
+    }
+    
+    entry->flags = entry->flags | MAPPED_BIT;
+
+    lock_release(&buffer_cache_lock);
+    
+    if(write_back) {
+      // Write data to device
+      struct block_operations* ops = prev_block->provider;
+      ASSERT(prev_block->type != BLOCK_FOREIGN);
+      ops->write(prev_block->aux, prev_sector, entry->data);
+      prev_block->write_cnt++;
+    }
+
+    if(coherent && must_evict) {
+      // Read data from device
+      struct block_operations* ops = block->provider;
+      ops->read(block->aux, sector, entry->data);
+      block->read_cnt++;
+    }
+
+    result = entry->data;
   }
 
-  while(entry->flags & MAPPED_BIT)
-    cond_wait(&entry->cond, &buffer_cache_lock);
-
-  entry->flags = entry->flags | MAPPED_BIT;
-
-  lock_release(&buffer_cache_lock);
-
-  struct block_operations* ops = block->provider;
-
-  if(write_back) {
-    check_sector(block, sector);
-    ASSERT(block->type != BLOCK_FOREIGN);
-    ops->write(block->aux, prev_sector, entry->data);
-    block->write_cnt++;
-  }
-
-  if(coherent && must_evict) {
-    check_sector(block, sector);
-    ops->read(block->aux, sector, entry->data);
-    block->read_cnt++;
-  }
-
-  return entry->data;
+  return result;
 }
 
 void block_unmap_sector(struct block* block, block_sector_t sector, bool dirty) {
@@ -214,10 +228,15 @@ void block_unmap_sector(struct block* block, block_sector_t sector, bool dirty) 
 
   lock_acquire(&buffer_cache_lock);
 
-  bool must_evict;
-  struct buffer_cache_entry* entry = find_entry(block, sector, &must_evict);
+  struct buffer_cache_entry* entry = NULL;
+  for(int i = 0; i < BUFFER_CACHE_SIZE; i++) {
+    if(buffer_cache[i].block == block && buffer_cache[i].sector == sector && buffer_cache[i].flags & MAPPED_BIT) {
+      entry = &buffer_cache[i];
+      break;
+    }
+  }
 
-  if(must_evict || entry == NULL)
+  if(entry == NULL)
     PANIC("Trying to unmap block sector that is not mapped!");
 
   entry->flags = entry->flags & (~MAPPED_BIT);
